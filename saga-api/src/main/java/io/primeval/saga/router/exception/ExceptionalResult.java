5a74e67b7d5a3ca231dbce27636d7f32bf7a9ae0
Simon Chemouil <simon.chemouil@lambdacube.fr>
2017-05-08 22:09:08 +0200

    Issue #3: added toString() to ExceptionalResult

--- saga-api/src/main/java/io/primeval/saga/router/exception/ExceptionalResult.java
+++ saga-api/src/main/java/io/primeval/saga/router/exception/ExceptionalResult.java
@@ -1,6 +1,8 @@
 package io.primeval.saga.router.exception;
 
-import io.primeval.saga.http.protocol.HttpMethod;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
 import io.primeval.saga.http.protocol.HttpRequest;
 
 public final class ExceptionalResult {
@@ -14,4 +16,11 @@ public final class ExceptionalResult {
         this.throwable = throwable;
     }
 
+    @Override
+    public String toString() {
+        StringWriter sw = new StringWriter();
+        PrintWriter pw = new PrintWriter(sw);
+        throwable.printStackTrace(pw);
+        return "An exception happened on " + httpRequest.method + " " + httpRequest.uri + ": " + sw.toString();
+    }
 }
